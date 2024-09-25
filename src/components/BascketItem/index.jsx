e6b0eecc4e0d59cab1612ac63ba04db30f9da6c2
VallyL <levin.valeriia01@gmail.com>
2024-09-25 19:45:39 +0200

    basket done

--- src/components/BascketItem/index.jsx
+++ src/components/BascketItem/index.jsx
@@ -1,18 +1,25 @@
 import { useDispatch } from "react-redux";
 import { removeFromCart } from "../../redux/actions/action";
+import BasketImg from "../../assets/icons/Vector copy 3.svg";
+import styles from "../../components/BascketItem/styles.module.css";
 
 function BascketItem({ product }) {
   const dispatch = useDispatch();
 
   return (
-    <nav>
+    <nav className={styles.itemContainer}>
       <img src={product.image} alt={product.name} />
       <hr></hr>
-      <div>
+      <div className={styles.nameDiv}>
         <h3>{product.name}</h3>
-        <p>PRICE: {product.price}$</p>
+        <div>
+          <p>PRICE:</p>
+          <h3>{product.price}$</h3>
+        </div>
       </div>
-      <button onClick={() => dispatch(removeFromCart(product.id))}>-</button>
+      <button onClick={() => dispatch(removeFromCart(product.id))}>
+        <img src={BasketImg} alt="delete" />
+      </button>
     </nav>
   );
 }
--- src/pages/Bascket/index.jsx
+++ src/pages/Bascket/index.jsx
@@ -12,19 +12,32 @@ function Bascket() {
     dispatch(fetchCart());
   }, [dispatch]);
 
+  const totalSum = cart.reduce((acc, item) => acc + parseFloat(item.price), 0);
+  const itemNames = cart.map((item) => item.name);
+
   return (
     <section className={styles.bascketContainer}>
       <h1>Basket</h1>
       <hr></hr>
-      <nav>
-        <div>
+      <nav className={styles.cartContainer}>
+        <div className={styles.listDiv}>
           {cart.length === 0 ? (
             <p>Your basket is empty ...</p>
           ) : (
             cart.map((item) => <BascketItem key={item.id} product={item} />)
           )}
         </div>
-        <div></div>
+        <div className={styles.totalContainer}>
+          <h1>Total</h1>
+          <ul>
+            {itemNames.map((name, index) => (
+              <li key={index}>{name}</li>
+            ))}
+          </ul>
+          <hr></hr>
+          <p>PRICE:</p>
+          <h2>{totalSum.toFixed(2)} $</h2>
+        </div>
       </nav>
     </section>
   );
